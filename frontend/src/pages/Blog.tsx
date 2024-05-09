1697734022b50ae70a090c270683bedbcc4db0cc
Muhammad Faizan Abbasi <faizanx168@gmail.com>
2024-05-09 00:03:01 -0400

    Type errors

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -7,7 +7,7 @@ export const Blog = () => {
   const { id } = useParams();
   const { loading, blog } = useBlog({ id: id || "" });
 
-  if (loading) {
+  if (loading || !blog) {
     return (
       <>
         <Appbar />
